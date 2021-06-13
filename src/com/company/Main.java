cb85bed9b5963579a08b12632965c0fa8070a55b
amira2013 <drisschima42@gmail.com>
2021-06-13 12:37:44 -0400

    push u origin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+     SolarSystem x=new SolarSystem();
+     x.sun=1;
+     x.moon=2;
+     x.planets=3;
 
     }
 }
