a2c80e9138f2e54748e6e2c7825c3430e9e521e3
amira2013 <drisschima42@gmail.com>
2021-06-13 12:52:40 -0400

    Featursun with an unfixed error

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,13 @@ public class Main {
      x.sun=1;
      x.moon=2;
      x.planets=3;
+     sun Feron =new sun();
+        Feron.heat="600000000000kj";
+        Feron.radius="823587325872385723895km/2";
+        Feron.Temperature="100000000000000000000000000f";
+
+
+
 
     }
 }
